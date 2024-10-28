77f6f212b00d4123efeaefe7c2ab6ec40f2d34ee
Tim Swodenk <tim.swodenk@web.de>
2024-10-28 20:45:44 +0100

    feat: Add search function, add Pokemon type and uppercase Pokemon names

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let maxLoadedPokemon = 20;
+let maxLoadedPokemon = 1;
 
 const BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=${maxLoadedPokemon}&offset=0`;
 
+let allPokemonDetails = [];
 let colours = {
   normal: "#A8A77A",
   fire: "#EE8130",
@@ -23,6 +24,27 @@ let colours = {
   fairy: "#D685AD",
 };
 
+let types = {
+  normal: "./assets/pokemon-type-svg-icons-master/icons/normal.svg",
+  fire: "./assets/pokemon-type-svg-icons-master/icons/fire.svg",
+  water: "./assets/pokemon-type-svg-icons-master/icons/water.svg",
+  electric: "./assets/pokemon-type-svg-icons-master/icons/electric.svg",
+  grass: "./assets/pokemon-type-svg-icons-master/icons/grass.svg",
+  ice: "./assets/pokemon-type-svg-icons-master/icons/ice.svg",
+  fighting: "./assets/pokemon-type-svg-icons-master/icons/fighting.svg",
+  poison: "./assets/pokemon-type-svg-icons-master/icons/poison.svg",
+  ground: "./assets/pokemon-type-svg-icons-master/icons/ground.svg",
+  flying: "./assets/pokemon-type-svg-icons-master/icons/flying.svg",
+  psychic: "./assets/pokemon-type-svg-icons-master/icons/psychic.svg",
+  bug: "./assets/pokemon-type-svg-icons-master/icons/bug.svg",
+  rock: "./assets/pokemon-type-svg-icons-master/icons/rock.svg",
+  ghost: "./assets/pokemon-type-svg-icons-master/icons/ghost.svg",
+  dragon: "./assets/pokemon-type-svg-icons-master/icons/dragon.svg",
+  dark: "./assets/pokemon-type-svg-icons-master/icons/dark.svg",
+  steel: "./assets/pokemon-type-svg-icons-master/icons/steel.svg",
+  fairy: "./assets/pokemon-type-svg-icons-master/icons/fairy.svg",
+};
+
 async function loadData() {
   try {
     let data = await getPokemons();
@@ -32,6 +54,7 @@ async function loadData() {
     for (let pokemon of pokemons) {
       try {
         let details = await fetchPokemonDetails(pokemon.url);
+        allPokemonDetails.push(details);
         renderPokemon(details);
       } catch (error) {
         console.error("Fehler beim Laden der Pokémon-Details:", error);
@@ -59,20 +82,89 @@ async function fetchPokemonDetails(url) {
 }
 
 function renderPokemon(pokemon) {
+  let pokemonName = pokemon.forms[0].name;
+  let pokemonImage = pokemon.sprites.front_default;
+  let pokemonId = pokemon.id;
+  let bgColor = colours[pokemon.types[0].type.name] || "#FFF";
+  let imgPokemonType1 = types[pokemon.types[0].type.name];
+  let pokemonType1 = pokemon.types[0].type.name;
+  let hasTwoTypes = false;
+  let imgPokemonType2 = null;
+  let pokemonType2 = null;
+
   console.log(pokemon);
 
-  let content = document.getElementById("layout");
-  const pokemonName = pokemon.forms[0].name;
-  const pokemonImage = pokemon.sprites.front_default;
-  const pokemonId = pokemon.id;
-  const bgColor = colours[pokemon.types[0].type.name] || "#FFF";
+  if (pokemon.types.length == 2) {
+    imgPokemonType2 = types[pokemon.types[1].type.name];
+    pokemonType2 = pokemon.types[1].type.name;
+    hasTwoTypes = true;
+  }
 
+  renderHtml(
+    pokemonName,
+    pokemonImage,
+    pokemonId,
+    bgColor,
+    imgPokemonType1,
+    pokemonType1,
+    imgPokemonType2,
+    pokemonType2,
+    hasTwoTypes
+  );
+}
+function firstLetterCap(pokemonName) {
+  let capitalized = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+  return capitalized;
+}
+
+function searchPokemon(query) {
+  query = query.toLowerCase();
+  document.getElementById("layout").innerHTML = "";
+
+  let filteredPokemons = allPokemonDetails.filter((pokemon) => {
+    return (
+      pokemon.forms[0].name.toLowerCase().startsWith(query) ||
+      `#${pokemon.id}`.startsWith(query)
+    );
+  });
+
+  filteredPokemons.forEach((pokemon) => renderPokemon(pokemon));
+}
+
+function renderHtml(
+  pokemonName,
+  pokemonImage,
+  pokemonId,
+  bgColor,
+  imgPokemonType1,
+  pokemonType1,
+  imgPokemonType2,
+  pokemonType2,
+  hasTwoTypes
+) {
+  let content = document.getElementById("layout");
   content.innerHTML += /*html*/ `
       <div class="col">
         <div class="p-3" style="background-color: ${bgColor}">
-            <div>#${pokemonId}</div>
-          <img src="${pokemonImage}" alt="">
-          <div>${pokemonName}</div>
+          <img src="./assets/logo/clipart2514739.png" class="background-image">
+          <div>#${pokemonId}</div>
+          <div class="type-wrapper">
+            <div class="icon ${pokemonType1}">
+              <img src="${imgPokemonType1}"/>
+            </div>
+            ${
+              hasTwoTypes
+                ? `
+            <div class="icon ${pokemonType2}">
+              <img src="${imgPokemonType2}"/>
+            </div>
+          `
+                : ""
+            }
+          </div>
+          <img class="font-image" src="${pokemonImage}" alt="">
+          <div>${firstLetterCap(pokemonName)}</div>
         </div>
       </div>
     `;
